811d012ad7ebb65757c680c8b710866583170a3d
Really Him <hesreallyhim@proton.me>
2025-11-24 18:10:07 -0500

    chore: undo changes prior to draft

--- schema/2025-03-26/schema.ts
+++ schema/2025-03-26/schema.ts
@@ -1119,8 +1119,6 @@ export interface CompleteResult extends Result {
   completion: {
     /**
      * An array of completion values. Must not exceed 100 items.
-     *
-     * @maxItems 100
      */
     values: string[];
     /**
--- schema/2025-06-18/schema.ts
+++ schema/2025-06-18/schema.ts
@@ -1355,8 +1355,6 @@ export interface CompleteResult extends Result {
   completion: {
     /**
      * An array of completion values. Must not exceed 100 items.
-     *
-     * @maxItems 100
      */
     values: string[];
     /**
