700542668687d14b7bf6d7a0d269b95bef36c622
Caitie McCaffrey <caitiem20@github.com>
2026-03-09 17:22:22 -0700

    make ResultType default to complete

--- schema/draft/schema.ts
+++ schema/draft/schema.ts
@@ -137,6 +137,8 @@ export interface Result {
   /**
    * Indicates the type of the result, which allows the client to determine
    * how to parse the result object.
+   *
+   * @default "complete"
    */
   result_type?: ResultType;
   [key: string]: unknown;
